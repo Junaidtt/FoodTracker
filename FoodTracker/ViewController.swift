1edcf41fa73143732c7b6b853c0e36dd5847b227
Junaid <junaid@ibookholiday.com>
2019-02-07 14:04:12 +0400

    Connect the UI to code

--- FoodTracker/ViewController.swift
+++ FoodTracker/ViewController.swift
@@ -8,21 +8,42 @@
 
 import UIKit
 
-class ViewController: UIViewController {
+class ViewController: UIViewController,UITextFieldDelegate {
 
+    //Mark:Properties
+    @IBOutlet weak var naemTextField: UITextField!
+    @IBOutlet weak var mealnameLabel: UILabel!
+    
     override func viewDidLoad() {
         super.viewDidLoad()
         
         // Do any additional setup after loading the view, typically from a nib.
         
+        naemTextField.delegate = self
+        
         print("viewDidLoad")
     }
     
-    override func didReceiveMemoryWarning() {
-        super.didReceiveMemoryWarning()
+    //MARK: Actions
+    @IBAction func setDefaultTextLabel(_ sender: Any) {
         
-        print("didReceiveMemoryWarning")
+        mealnameLabel.text = "Default Text"
+        
     }
+    
+    // Mark: Protocols
+    func textFieldShouldReturn(_ textField: UITextField) -> Bool {
+        
+        naemTextField.resignFirstResponder()
+        return true
+    }
+    
+    func textFieldDidEndEditing(_ textField: UITextField) {
+        
+        mealnameLabel.text = textField.text
+    }
+    
+    
     
 
 }
